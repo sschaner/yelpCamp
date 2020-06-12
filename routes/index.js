6b18e11a053c9a34c0733ee7dbf3c8eff8608629
Steven Schaner <schanerst@gmail.com>
2020-06-12 17:38:26 -0400

    change from email to heroku email in forgot routes

--- routes/index.js
+++ routes/index.js
@@ -115,143 +115,143 @@ router.get("/logout", (req, res) => {
 });
 
 // FORGOT PASSWORD ROUTE
-// router.get("/forgot", (req, res) => {
-//   res.render("forgot");
-// });
+router.get("/forgot", (req, res) => {
+  res.render("forgot");
+});
 
-// router.post("/forgot", (req, res, next) => {
-//   async.waterfall(
-//     [
-//       (done) => {
-//         crypto.randomBytes(20, (err, buf) => {
-//           var token = buf.toString("hex");
-//           done(err, token);
-//         });
-//       },
+router.post("/forgot", (req, res, next) => {
+  async.waterfall(
+    [
+      (done) => {
+        crypto.randomBytes(20, (err, buf) => {
+          var token = buf.toString("hex");
+          done(err, token);
+        });
+      },
 
-//       (token, done) => {
-//         User.findOne({ email: req.body.email }, (err, user) => {
-//           if (!user) {
-//             req.flash("error", "No account with that email address exists.");
-//             return res.redirect("/forgot");
-//           }
+      (token, done) => {
+        User.findOne({ email: req.body.email }, (err, user) => {
+          if (!user) {
+            req.flash("error", "No account with that email address exists.");
+            return res.redirect("/forgot");
+          }
 
-//           user.resetPasswordToken = token;
-//           user.resetPasswordExpires = Date.now() + 3600000; // 1 hour
+          user.resetPasswordToken = token;
+          user.resetPasswordExpires = Date.now() + 3600000; // 1 hour
 
-//           user.save((err) => {
-//             done(err, token, user);
-//           });
-//         });
-//       },
+          user.save((err) => {
+            done(err, token, user);
+          });
+        });
+      },
 
-//       (token, user, done) => {
-//         var mailOptions = {
-//           from: "app174367248@heroku.com",
-//           to: user.email,
-//           subject: "Vacation Tracker Password Reset",
-//           text:
-//             "You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n" +
-//             "Please click on the following link, or paste this into your browser to complete the process:\n\n" +
-//             "http://" +
-//             req.headers.host +
-//             "/reset/" +
-//             token +
-//             "\n\n" +
-//             "If you did not request this, please ignore this email and your password will remain unchanged.\n",
-//         };
-//         mailgun.messages().send(mailOptions, (err) => {
-//           req.flash(
-//             "success",
-//             "An e-mail has been sent to " +
-//               user.email +
-//               " with further instructions."
-//           );
-//           done(err, "done");
-//         });
-//       },
-//     ],
-//     (err) => {
-//       if (err) return next(err);
-//       res.redirect("/forgot");
-//     }
-//   );
-// });
+      (token, user, done) => {
+        var mailOptions = {
+          from: "app174367248@heroku.com",
+          to: user.email,
+          subject: "Vacation Tracker Password Reset",
+          text:
+            "You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n" +
+            "Please click on the following link, or paste this into your browser to complete the process:\n\n" +
+            "http://" +
+            req.headers.host +
+            "/reset/" +
+            token +
+            "\n\n" +
+            "If you did not request this, please ignore this email and your password will remain unchanged.\n",
+        };
+        mailgun.messages().send(mailOptions, (err) => {
+          req.flash(
+            "success",
+            "An e-mail has been sent to " +
+              user.email +
+              " with further instructions."
+          );
+          done(err, "done");
+        });
+      },
+    ],
+    (err) => {
+      if (err) return next(err);
+      res.redirect("/forgot");
+    }
+  );
+});
 
-// router.get("/reset/:token", (req, res) => {
-//   User.findOne(
-//     {
-//       resetPasswordToken: req.params.token,
-//       resetPasswordExpires: { $gt: Date.now() },
-//     },
-//     (err, user) => {
-//       if (!user) {
-//         req.flash("error", "Password reset token is invalid or has expired.");
-//         return res.redirect("/forgot");
-//       }
-//       res.render("reset", { token: req.params.token });
-//     }
-//   );
-// });
+router.get("/reset/:token", (req, res) => {
+  User.findOne(
+    {
+      resetPasswordToken: req.params.token,
+      resetPasswordExpires: { $gt: Date.now() },
+    },
+    (err, user) => {
+      if (!user) {
+        req.flash("error", "Password reset token is invalid or has expired.");
+        return res.redirect("/forgot");
+      }
+      res.render("reset", { token: req.params.token });
+    }
+  );
+});
 
-// router.post("/reset/:token", (req, res) => {
-//   async.waterfall(
-//     [
-//       (done) => {
-//         User.findOne(
-//           {
-//             resetPasswordToken: req.params.token,
-//             resetPasswordExpires: { $gt: Date.now() },
-//           },
-//           (err, user) => {
-//             if (!user) {
-//               req.flash(
-//                 "error",
-//                 "Password reset token is invalid or has expired."
-//               );
-//               return res.redirect("back");
-//             }
-//             if (req.body.password === req.body.confirm) {
-//               user.setPassword(req.body.password, (err) => {
-//                 user.resetPasswordToken = undefined;
-//                 user.resetPasswordExpires = undefined;
+router.post("/reset/:token", (req, res) => {
+  async.waterfall(
+    [
+      (done) => {
+        User.findOne(
+          {
+            resetPasswordToken: req.params.token,
+            resetPasswordExpires: { $gt: Date.now() },
+          },
+          (err, user) => {
+            if (!user) {
+              req.flash(
+                "error",
+                "Password reset token is invalid or has expired."
+              );
+              return res.redirect("back");
+            }
+            if (req.body.password === req.body.confirm) {
+              user.setPassword(req.body.password, (err) => {
+                user.resetPasswordToken = undefined;
+                user.resetPasswordExpires = undefined;
 
-//                 user.save((err) => {
-//                   req.logIn(user, (err) => {
-//                     done(err, user);
-//                   });
-//                 });
-//               });
-//             } else {
-//               req.flash("error", "Passwords do not match.");
-//               return res.redirect("back");
-//             }
-//           }
-//         );
-//       },
+                user.save((err) => {
+                  req.logIn(user, (err) => {
+                    done(err, user);
+                  });
+                });
+              });
+            } else {
+              req.flash("error", "Passwords do not match.");
+              return res.redirect("back");
+            }
+          }
+        );
+      },
 
-//       (user, done) => {
-//         var mailOptions = {
-//           from: "app174367248@heroku.com",
-//           to: user.email,
-//           subject: "Your Vacation Tracker Password Has Been Changed",
-//           text:
-//             "Hello,\n\n" +
-//             "This is a confirmation that the password for your account " +
-//             user.email +
-//             " has just been changed.\n",
-//         };
-//         mailgun.messages().send(mailOptions, (err) => {
-//           req.flash("success", "Your password has been changed.");
-//           done(err);
-//         });
-//       },
-//     ],
-//     (err) => {
-//       res.redirect("/campgrounds");
-//     }
-//   );
-// });
+      (user, done) => {
+        var mailOptions = {
+          from: "app174367248@heroku.com",
+          to: user.email,
+          subject: "Your Vacation Tracker Password Has Been Changed",
+          text:
+            "Hello,\n\n" +
+            "This is a confirmation that the password for your account " +
+            user.email +
+            " has just been changed.\n",
+        };
+        mailgun.messages().send(mailOptions, (err) => {
+          req.flash("success", "Your password has been changed.");
+          done(err);
+        });
+      },
+    ],
+    (err) => {
+      res.redirect("/campgrounds");
+    }
+  );
+});
 
 // USER PROFILES ROUTE
 router.get("/users/:id", (req, res) => {
